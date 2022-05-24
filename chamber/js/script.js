c83b7273f2836bc4e88c043ad45d028601e7d920
Beccalynne <nor12012@byui.edu>
2022-05-24 12:21:09 -0600

    All but medium css work and no js

---
+++ chamber/js/script.js
@@ -0,0 +1,11 @@
+document.querySelector("#lastmod").textContent = document.lastModified;
+try{
+    let options = {
+        year: "numeric"
+    }
+    document.getElementById("#currentyear").textContent = new Date().toLocaleDateString("en-US", options);
+} catch (e) {
+    alert("Error with code in browser");
+}
+const currentYear = "2022";
+document.querySelector('#year').textContent = currentYear
