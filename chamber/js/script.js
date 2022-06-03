9cff1b8a12631cc5194d9366074e4f0fac47e479
Beccalynne <nor12012@byui.edu>
2022-06-03 11:14:11 -0600

    min requirements wk 7 done

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -23,41 +23,59 @@ https://www.w3schools.com/js/js_arrow_function.asp
 window.onresize = () => {if (window.innerWidth > 760) {mainnav.classList.remove('responsive')}};
 
 
+
 // Lasy Loading
 
 let imagesToLoad = document.querySelectorAll('img[data-src]');
-
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+// Options passed to the Intersection Observer
 const imgOptions = {
-    threshold: 3,
-    rootMargin: "0px 0px 50px 50px"
+  threshold: 1,
+  /* Using -10px for demo purposes, normally use something like 300px so 
+     the images load well before the user scrolls to them */
+  rootMargin: "0px 0px 500px 0px"
+}
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
 }
 
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
-};
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
+// Last Modified
 
-if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
-  
+let lastVisitDate;
+const now = Date.now();
+
+if (window.localStorage.getItem('last-visit-date'))
+{
+    lastVisitDate = window.localStorage.getItem('last-visit-date');
+} else{
+    lastVisitDate = now;
+}
+
+window.localStorage.setItem('last-visit-date', now);
+
+const secondsSinceLastVisit = Math.floor((now - lastVisitDate) / 1000);
+const daysSinceLastVisit = Math.floor(secondsSinceLastVisit / 86400);
+console.log(daysSinceLastVisit);
+
+// document.querySelector('#seconds-ago').textContent = secondsSinceLastVisit;
+document.querySelector('#days-ago').textContent = daysSinceLastVisit
