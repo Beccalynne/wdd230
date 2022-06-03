fa743bd198c561c10cb99eaab4913723bf960585
Beccalynne <nor12012@byui.edu>
2022-06-03 09:08:45 -0600

    Lazy load fix

--- lesson7/script.js
+++ lesson7/script.js
@@ -1,36 +1,102 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
+// let imagesToLoad = document.querySelectorAll('img[data-src]');
 
-const imgOptions = {
-    threshold: 3,
-    rootMargin: "0px 0px 50px 50px"
-}
+// const imgOptions = {
+//     threshold: '',
+//     rootMargin: "0px 0px 50px 50px"
+// }
 
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
+// const loadImages = (image) => {
+//     image.setAttribute('src', image.getAttribute('data-src'));
+//     image.onload = () => {
+//         image.removeAttribute('data-src');
+//     };
+// };
+
+// imagesToLoad.forEach((img) => {
+//     loadImages(img);
+// });
+
+// if('IntersectionObserver' in window) {
+//     const observer = new IntersectionObserver((items, observer) => {
+//       items.forEach((item) => {
+//         if(item.isIntersecting) {
+//           loadImages(item.target);
+//           observer.unobserve(item.target);
+//         }
+//       });
+//     });
+//     imagesToLoad.forEach((img) => {
+//       observer.observe(img);
+//     });
+//   } else {
+//     imagesToLoad.forEach((img) => {
+//       loadImages(img);
+//     });
+//   }
+//       let imagesToLoad = document.querySelectorAll('img[data-src]');
+//     const loadImages = (image) => {
+//       image.setAttribute('src', image.getAttribute('data-src'));
+//       image.onload = () => {
+//         image.removeAttribute('data-src');
+//       };
+//     };
+//     // Options passed to the Intersection Observer
+//     const imgOptions = {
+//       threshold: 1,
+//       /* Using -10px for demo purposes, normally use something like 300px so 
+//          the images load well before the user scrolls to them */
+//       rootMargin: "0px 0px 500px 0px"
+//     }
+//     if ('IntersectionObserver' in window) {
+//       const observer = new IntersectionObserver((items, observer) => {
+//         items.forEach((item) => {
+//           if (item.isIntersecting) {
+//             loadImages(item.target);
+//             observer.unobserve(item.target);
+//           }
+//         });
+//       }, imgOptions);
+//       imagesToLoad.forEach((img) => {
+//         observer.observe(img);
+//       });
+//     } else {
+//       imagesToLoad.forEach((img) => {
+//         loadImages(img);
+//       });
+//     }
+
+
+
+
+
+    let imagesToLoad = document.querySelectorAll('img[data-src]');
+    const loadImages = (image) => {
+      image.setAttribute('src', image.getAttribute('data-src'));
+      image.onload = () => {
         image.removeAttribute('data-src');
+      };
     };
-};
-
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
-
-if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
+    // Options passed to the Intersection Observer
+    const imgOptions = {
+      threshold: 1,
+      /* Using -10px for demo purposes, normally use something like 300px so 
+         the images load well before the user scrolls to them */
+      rootMargin: "0px 0px 500px 0px"
+    }
+    if ('IntersectionObserver' in window) {
+      const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+          if (item.isIntersecting) {
+            loadImages(item.target);
+            observer.unobserve(item.target);
+          }
+        });
+      }, imgOptions);
+      imagesToLoad.forEach((img) => {
+        observer.observe(img);
       });
-    });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
-  
+    } else {
+      imagesToLoad.forEach((img) => {
+        loadImages(img);
+      });
+    }
