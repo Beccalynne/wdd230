9276be629f9d189128f75be6d6904bad9b6f5c04
Beccalynne <nor12012@byui.edu>
2022-06-23 13:37:27 -0600

    updated directory

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,7 +1,7 @@
 fetch ('js/data.json')
 const cards = document.querySelector('.cards');
 
-fetch(requestURL)
+fetch('js/data.json')
   .then(function (response) {
     return response.json();
   })
@@ -17,32 +17,55 @@ fetch(requestURL)
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let portrait = document.createElement('img');
-    let birthdate = document.createElement('p');
-    let birthplace = document.createElement('p');
-    let order = document.createElement('p')
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let url = document.createElement('p');
+    let membership = document.createElement('p');
   
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = companies.name + ' ' + companies.lastname;
+    h2.textContent = companies.name;
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', companies.imageurl);
-    portrait.setAttribute('alt', 'Portait of ' + companies.name + ' ' + companies.lastname);
+    portrait.setAttribute('alt', 'Logo of ' + companies.name);
     portrait.setAttribute('loading', 'lazy');
 
-    birthdate.textContent = `Birthdate: ${companies.birthdate}`;
-    birthplace.textContent = `Birthplace: ${companies.birthplace}`;
-    order.textContent = `${companies.order}`;
+    address.textContent = `${companies.address}`;
+    phone.textContent = `${companies.phone}`;
+    url.textContent = `${companies.url}`;
+    membership.textContent = `Membership level: ${companies.membership}`;
 
   
     // Add/append the section(card) with the h2 element
     // order depends how it will show up
     card.appendChild(h2);
-    card.appendChild(birthdate);
-    card.appendChild(birthplace);
+    card.appendChild(membership);
+    card.appendChild(address);
+    card.appendChild(phone);
     card.appendChild(portrait);
-    card.appendChild(order);
+    card.appendChild(url);
 
   
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
+  }
+
+  // Grid -- List view
+  const gridbutton = document.querySelector("#grid");
+  const listbutton = document.querySelector("#list");
+  const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+  gridbutton.addEventListener("click", () => {
+	  // example using arrow function
+	  display.classList.add("grid");
+	  display.classList.remove("list");
+  });
+
+  listbutton.addEventListener("click", showList); // example using defined function
+
+  function showList() {
+	  display.classList.add("list");
+	  display.classList.remove("grid");
   }
